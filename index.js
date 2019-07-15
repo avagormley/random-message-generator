815decbcd257b8df376e2f43694a6070200b88b2
Apple <apple@Apples-MacBook-Pro-79.local>
2019-07-15 09:22:47 -0700

    v1

---
+++ index.js
@@ -0,0 +1,31 @@
+var button = $('.fortune');
+button.on('click', print);
+function doSomething () {
+  console.log ("You clicked the button")
+}
+
+var fortunes = ["You are going to meet someone special today.",
+                "One of your dreams will come true.",
+                "Be patient. Everything will eventually work out.",
+                "Don't let what you can't do interfere with what you can do.",
+                "Success is never wondering what if.",
+                "Obstacles are those frightful things you see when you take your eyes off your goals.",
+                "Happiness is not a destination. It is a method of life.",
+                "If it seems fates are against youtoday, they probably are.",
+                "The fortune you seek is in another cookie.",
+                "Stop procrastonatong - starting tomorrow.",
+                "A foolish man listens to his heart. A wise man listens to cookies.",
+                "I see money in your future... It is not yours though.",
+                "Next week at this time, something good is coming your way.",
+                "Ignore previous cookie."]
+
+function selectrand(){
+  var randomDecimal = Math.random();
+  var random = randomDecimal * fortunes.length;
+  var final = Math.floor(random);
+
+  console.log(fortunes[final]);
+}
+document.getElementsByClassName('fortune').innerHTML = "Paragraph changed!";
+
+console.log(selectrand());
